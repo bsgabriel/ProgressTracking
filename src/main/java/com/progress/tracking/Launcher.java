a643afa15999d5f41364f0d130b032da8d1d12f7
bsgabriel <gabrielbs22@icloud.com>
2024-03-19 00:29:11 -0300

    Removed unnecessary code

--- src/main/java/com/progress/tracking/Launcher.java
+++ src/main/java/com/progress/tracking/Launcher.java
@@ -2,37 +2,12 @@ package com.progress.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
         SpringApplication.run(Launcher.class, args);
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public static void browse() {
-        if (true)
-            return;
-        System.setProperty("java.awt.headless", "false");
-
-        if (!Desktop.isDesktopSupported()) {
-            System.err.println("Desktop not supported!");
-            return;
-        }
-        Desktop desktop = Desktop.getDesktop();
-        try {
-            desktop.browse(new URI("http://localhost:8080"));
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace();
-        }
     }
 
 }
