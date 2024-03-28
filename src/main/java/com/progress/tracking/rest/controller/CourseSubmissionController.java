46cc893cabfe6d812f84933575bed4e622eb459e
bsgabriel <gabrielbs22@icloud.com>
2024-03-27 21:36:39 -0300

    Added Javadoc to submission endpoint controller.

--- src/main/java/com/progress/tracking/rest/controller/CourseSubmissionController.java
+++ src/main/java/com/progress/tracking/rest/controller/CourseSubmissionController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Endpoints for course submission.
+ */
 @RestController
 @RequestMapping("/submit")
 public class CourseSubmissionController {
@@ -17,6 +20,12 @@ public class CourseSubmissionController {
     @Autowired
     private CourseSubmissionService courseSubmission;
 
+    /**
+     * Endpoint to submit a course to Trello.
+     *
+     * @param req The {@linkplain CourseToTrelloRequest} containing the details of the course to be submitted.
+     * @return The {@linkplain CourseToTrelloResponse} containing information about the submission status.
+     */
     @PostMapping("/toTrello")
     public ResponseEntity<CourseToTrelloResponse> toTrello(@RequestBody CourseToTrelloRequest req) {
         return this.courseSubmission.submitToTrello(req);
