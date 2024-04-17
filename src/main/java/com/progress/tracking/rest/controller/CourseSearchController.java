51bfc43e500d99facc61fa3769d2906a3725de86
bsgabriel <gabrielbs22@icloud.com>
2024-04-17 19:36:11 -0300

    Fixed class annotation

--- src/main/java/com/progress/tracking/rest/controller/CourseSearchController.java
+++ src/main/java/com/progress/tracking/rest/controller/CourseSearchController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Endpoints for course search.
  */
-@RestController()
+@RestController
 @RequestMapping("/search")
 public class CourseSearchController {
 
