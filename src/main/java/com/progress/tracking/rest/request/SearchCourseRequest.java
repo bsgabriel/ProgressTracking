cfcab4b1185002df76418226e9478a85525c7429
bsgabriel <gabrielbs22@icloud.com>
2024-08-15 21:47:16 -0300

    Added lombok annotations to SearchCourseRequest

--- src/main/java/com/progress/tracking/rest/request/SearchCourseRequest.java
+++ src/main/java/com/progress/tracking/rest/request/SearchCourseRequest.java
@@ -1,49 +1,14 @@
 package com.progress.tracking.rest.request;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class SearchCourseRequest {
     private String udemyClientId;
     private String udemyClientSecret;
     private String course;
     private Integer max;
     private Integer page;
-
-    public String getUdemyClientId() {
-        return udemyClientId;
-    }
-
-    public void setUdemyClientId(String udemyClientId) {
-        this.udemyClientId = udemyClientId;
-    }
-
-    public String getUdemyClientSecret() {
-        return udemyClientSecret;
-    }
-
-    public void setUdemyClientSecret(String udemyClientSecret) {
-        this.udemyClientSecret = udemyClientSecret;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public Integer getMax() {
-        return max;
-    }
-
-    public void setMax(Integer max) {
-        this.max = max;
-    }
-
-    public Integer getPage() {
-        return page;
-    }
-
-    public void setPage(Integer page) {
-        this.page = page;
-    }
 }
