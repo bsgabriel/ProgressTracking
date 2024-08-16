9d5ccf988f1fb3ff88c689bc0a5519183f5e433f
bsgabriel <gabrielbs22@icloud.com>
2024-08-15 21:46:46 -0300

    Changed course search from POST to GET

--- src/main/java/com/progress/tracking/rest/controller/CourseSearchController.java
+++ src/main/java/com/progress/tracking/rest/controller/CourseSearchController.java
@@ -5,10 +5,7 @@ import com.progress.tracking.rest.response.SearchCourseResponse;
 import com.progress.tracking.rest.service.CourseSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Endpoints for course search.
@@ -23,12 +20,30 @@ public class CourseSearchController {
     /**
      * Endpoint to search for courses on Udemy platform.
      *
-     * @param req The {@linkplain SearchCourseRequest} containing search parameters.
-     * @return ResponseEntity containing search results.
+     * @param udemyClientId     The client ID for Udemy API.
+     * @param udemyClientSecret The client secret for Udemy API.
+     * @param course            The course name or keyword to search for.
+     * @param page              The page number for pagination.
+     * @param max               The maximum number of results per page.
+     * @return {@link SearchCourseResponse} containing search results.
      */
-    @PostMapping("/fromUdemy")
-    public ResponseEntity<SearchCourseResponse> fromUdemy(@RequestBody SearchCourseRequest req) {
-        return ResponseEntity.ok(this.courseSearch.searchFromUdemy(req));
+    @GetMapping("/fromUdemy")
+    public ResponseEntity<SearchCourseResponse> fromUdemy(
+            @RequestHeader("Udemy-Client-Id") String udemyClientId,
+            @RequestHeader("Udemy-Client-Secret") String udemyClientSecret,
+            @RequestParam String course,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "5") int max) {
+
+        var request = SearchCourseRequest.builder()
+                .udemyClientId(udemyClientId)
+                .udemyClientSecret(udemyClientSecret)
+                .course(course)
+                .max(max)
+                .page(page)
+                .build();
+
+        return ResponseEntity.ok(this.courseSearch.searchFromUdemy(request));
     }
 
 }
