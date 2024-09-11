a29a5c4eb75440571d9551de761599677b5c415a
bsgabriel <gabrielbs22@icloud.com>
2024-09-10 21:33:32 -0300

    Enabled FeignClient

--- src/main/java/com/progress/tracking/Launcher.java
+++ src/main/java/com/progress/tracking/Launcher.java
@@ -2,8 +2,10 @@ package com.progress.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Launcher {
 
     public static void main(String[] args) {
